de30248e6b4492c30298a06f023e18a91f91248f
VladislavTiglau <vladt199882@gmail.com>
2026-01-06 13:19:19 +0100

    lesson1

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,8 @@
+import "./styles.css";
+function Button({name, onButtonClick}) {
+  return (
+  <button className="button" onClick={onButtonClick}>{name}
+  </button>
+  );
+}
+export default Button;
